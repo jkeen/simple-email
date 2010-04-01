4a60b1d2c01b54949b386cdc1b4d590d4af5301f
Jeff Keen <jeff@jeffreykeen.com>
2010-03-31 23:38:39 -0500

    Add better validation, correct silly error

--- simple.rb
+++ simple.rb
@@ -3,6 +3,25 @@ require 'sinatra'
 require 'erb'
 require 'action_mailer'
 
+module RFC822
+  EmailAddress = begin
+    qtext = '[^\\x0d\\x22\\x5c\\x80-\\xff]'
+    dtext = '[^\\x0d\\x5b-\\x5d\\x80-\\xff]'
+    atom = '[^\\x00-\\x20\\x22\\x28\\x29\\x2c\\x2e\\x3a-' +
+      '\\x3c\\x3e\\x40\\x5b-\\x5d\\x7f-\\xff]+'
+    quoted_pair = '\\x5c[\\x00-\\x7f]'
+    domain_literal = "\\x5b(?:#{dtext}|#{quoted_pair})*\\x5d"
+    quoted_string = "\\x22(?:#{qtext}|#{quoted_pair})*\\x22"
+    domain_ref = atom
+    sub_domain = "(?:#{domain_ref}|#{domain_literal})"
+    word = "(?:#{atom}|#{quoted_string})"
+    domain = "#{sub_domain}(?:\\x2e#{sub_domain})*"
+    local_part = "#{word}(?:\\x2e#{word})*"
+    addr_spec = "#{local_part}\\x40#{domain}"
+    pattern = /\A#{addr_spec}\z/
+  end
+end
+
 helpers do
   include Rack::Utils
 end
@@ -11,12 +30,12 @@ get '/' do
   erb :index
 end
 
-get '/send'
+get '/send' do
   redirect '/'
 end
 
 post '/send' do
-  if params[:email] =~ /@/
+  if params[:email] =~ RFC822::EmailAddress
     @success = true;
   else
     @errors = "<span>Invalid email address</span>"
